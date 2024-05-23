885dc5ecea22493feefbf056e61b0676aaa82ff7
AshokGfsd <166788378+AshokGfsd@users.noreply.github.com>
2024-05-23 14:12:22 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ function App() {
     <>
       <span className="container">
         <Card k={"Card1"} price={0} mod={"FREE"} num={4} />
-        <Card key={"Card2"} price={0} mod={"PLUS"} num={7} />
-        <Card key={"Card3"} price={0} mod={"PRO"} num={8} />
+        <Card key={"Card2"} price={9} mod={"PLUS"} num={7} />
+        <Card key={"Card3"} price={49} mod={"PRO"} num={8} />
       </span>
     </>
   );
